32ab7252762ea2c138dbee3c4c3e345df278082c
solareenlo <solareenlo@gmail.com>
2018-11-11 11:57:57 +0900

    addでデータを書き込む処理を関数化

--- memo/notes.js
+++ memo/notes.js
@@ -2,20 +2,24 @@ console.log("notes.js稼働");
 
 const fs = require("fs");
 
-let addNote = (title, body) => {
-  let notes = [];
-  let note = {
-    title,
-    body
-  };
+let fetchNotes = () => {
   // 初めてデータを入力したときのエラー処理
   // note-data.jsonが無いときは新たにファイルを作り、
   // note-data.jsonがあるときはデータを付け加える
   try {
     let notesString = fs.readFileSync("note-data.json");
-    notes = JSON.parse(notesString);
+    return JSON.parse(notesString);
   } catch(err) {
+    return [];
   }
+};
+
+let addNote = (title, body) => {
+  let notes = fetchNotes();
+  let note = {
+    title,
+    body
+  };
   // titleが重複しているときのエラー処理
   // titleが重複していない時にだけ新たなデータを追記する
   // titleが重複しているときは何もしない
