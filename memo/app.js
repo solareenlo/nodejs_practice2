e816cbb0ca88af9253b0d08290d1a5f6e48ffd38
solareenlo <solareenlo@gmail.com>
2018-11-11 09:54:22 +0900

    仕切り直してスタート

--- memo/app.js
+++ memo/app.js
@@ -1,18 +1,5 @@
 console.log("app.js稼働開始");
 
 const fs = require("fs");
-const os = require("os");
 
 const notes = require("./notes.js");
-
-let user = os.userInfo();
-console.log(user);
-
-let result = notes.addNote();
-console.log(result);
-
-// fs.appendFile("greeting.txt", `Hello ${user.username}! あなたは${notes.age}歳です.\n`, function(err) {
-//   if(err) {
-//     console.log(err);
-//   }
-// });
