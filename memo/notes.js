aeb02d2bc61c7b8cb16fa58f601ccf9d92424af5
solareenlo <solareenlo@gmail.com>
2018-11-11 11:51:25 +0900

    addを行なった時に、titleが重複していない時にデータを追記する処理とかを追加

--- memo/notes.js
+++ memo/notes.js
@@ -8,8 +8,22 @@ let addNote = (title, body) => {
     title,
     body
   };
-  notes.push(note);
-  fs.writeFileSync("note-data.json", JSON.stringify(notes));
+  // 初めてデータを入力したときのエラー処理
+  // note-data.jsonが無いときは新たにファイルを作り、
+  // note-data.jsonがあるときはデータを付け加える
+  try {
+    let notesString = fs.readFileSync("note-data.json");
+    notes = JSON.parse(notesString);
+  } catch(err) {
+  }
+  // titleが重複しているときのエラー処理
+  // titleが重複していない時にだけ新たなデータを追記する
+  // titleが重複しているときは何もしない
+  let duplicatedNotes = notes.filter(note => note.title === title);
+  if(duplicatedNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("note-data.json", JSON.stringify(notes));
+  }
 };
 
 let showAll = () => {
